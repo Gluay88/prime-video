f96802a6a3388d325e6d8fbfd06dc7be3c978da0
gluay88 <p.wantagaew@gmail.com>
2022-04-13 15:31:41 -0700

    slider added

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener("click", e => {
     let handle
-    if(e.target.matches(".handle")){
+    if (e.target.matches(".handle")) {
         handle = e.target
     } else {
         handle = e.target.closest(".handle")
     } 
-    if(handle != null ) onHandleClick(handle)
+    if(handle != null) onHandleClick(handle)
 })
 
 function onHandleClick(handle){
@@ -13,12 +13,12 @@ function onHandleClick(handle){
     const sliderIndex = parseInt(
         getComputedStyle(slider).getPropertyValue("--slider-index")
     )
-    if(handle.classsList.contains("left-handle")){
+    if(handle.classList.contains("left-handle")){
         slider.style.setProperty("--slider-index", sliderIndex - 1)
 
     }
         
-    if(handle.classsList.contains("right-handle")){
+    if(handle.classList.contains("right-handle")){
         slider.style.setProperty("--slider-index", sliderIndex + 1)
     }
 }
